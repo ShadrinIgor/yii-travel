91704f21786fc382d03779f5d0d350b95eec3e2c
Igor <shadrin.igor@gmail.com>
2014-04-06 18:54:13 +0500

    -

--- protected/modules/user/components/UserController.php
+++ protected/modules/user/components/UserController.php
@@ -230,92 +230,95 @@ class UserController extends Controller
             if( !empty( $id ) )$item = $addClass::fetch( $id );
                           else $item = new $addClass();
 
-            if( property_exists( $item, "firm_id" ) && $item->firm_id )$firm = $item->firm_id;
-            if( !property_exists( $item, "firm_id" ) && $item->id )$firm = $item;
-
-            if( empty( $firm ) || $firm->id == 0 )
+            if( !$item->id || $item->user_id->id == Yii::app()->user->getId()  )
             {
-                $fid = (int)Yii::app()->request->getParam("fid", 0);
-                $firm = CatalogFirms::fetch( $fid );
-            }
+                if( property_exists( $item, "firm_id" ) && $item->firm_id )$firm = $item->firm_id;
+                if( !property_exists( $item, "firm_id" ) && $item->id )$firm = $item;
 
-            $message = ( !empty( $status ) && $status == 'saved' ) ? "Сохраненно" : "";
-
-            // Описание объявления
-            if( !empty( $_POST["update"] ) )
-            {
-                if( !$item->id )$isAdd = true;
-                    else $isAdd = false;
-
-                $item->setAttributesFromArray( $_POST[ $addClass ] );
-                //$item->is_resume = 0;
-                if( !$item->date )$item->date = time();
-                $item->user_id = Yii::app()->user->getId();
-                if( $item->save() )
+                if( empty( $firm ) || $firm->id == 0 )
                 {
-                    $this->redirect( SiteHelper::createUrl( "/user/".Yii::app()->controller->getId()."/description/", array("id"=>$item->id, "fid"=>$firm->id, "status"=>"saved") ) );
-                    die;
-                    //if( !$isAdd )$message = "Описание успешно обновленно";
-                    //        else $message = "Запись успешно добавлена";
+                    $fid = (int)Yii::app()->request->getParam("fid", 0);
+                    $firm = CatalogFirms::fetch( $fid );
                 }
-//                    else $message = "Произошла ошибка обновления описания";
-            }
 
-            $action = Yii::app()->request->getParam( "action" );
-            $gall_id = (int) Yii::app()->request->getParam( "gall_id", 0 );
-            $comMessage = "";
-            $gallMessage = "";
-            if( !empty($gallError) )$message = $gallError;
-            // Удаление фотографии
-            if( !empty($action) && $gall_id>0 )
-            {
-                $comModel = CatGallery::fetch( $gall_id );
-                if( $comModel->id >0 && $comModel->item_id == $item->id )
+                $message = ( !empty( $status ) && $status == 'saved' ) ? "Сохраненно" : "";
+
+                // Описание объявления
+                if( !empty( $_POST["update"] ) )
                 {
-                    if( $action == "delGallery" )
+                    if( !$item->id )$isAdd = true;
+                        else $isAdd = false;
+
+                    $item->setAttributesFromArray( $_POST[ $addClass ] );
+                    //$item->is_resume = 0;
+                    if( !$item->date )$item->date = time();
+                    $item->user_id = Yii::app()->user->getId();
+                    if( $item->save() )
                     {
-                        $comModel->delete();
-                        $gallMessage = "Картинка удалена";
+                        $this->redirect( SiteHelper::createUrl( "/user/".Yii::app()->controller->getId()."/description/", array("id"=>$item->id, "fid"=>$firm->id, "status"=>"saved") ) );
+                        die;
+                        //if( !$isAdd )$message = "Описание успешно обновленно";
+                        //        else $message = "Запись успешно добавлена";
+                    }
+    //                    else $message = "Произошла ошибка обновления описания";
+                }
+
+                $action = Yii::app()->request->getParam( "action" );
+                $gall_id = (int) Yii::app()->request->getParam( "gall_id", 0 );
+                $comMessage = "";
+                $gallMessage = "";
+                if( !empty($gallError) )$message = $gallError;
+                // Удаление фотографии
+                if( !empty($action) && $gall_id>0 )
+                {
+                    $comModel = CatGallery::fetch( $gall_id );
+                    if( $comModel->id >0 && $comModel->item_id == $item->id )
+                    {
+                        if( $action == "delGallery" )
+                        {
+                            $comModel->delete();
+                            $gallMessage = "Картинка удалена";
+                        }
                     }
                 }
-            }
 
-            $comm_id = (int) Yii::app()->request->getParam( "comm_id", 0 );
-            if( !empty($action) && $comm_id>0 )
-            {
-                $comModel = CatComments::fetch( $comm_id );
-                if( $comModel->id >0 && $comModel->item_id->id == $item->id )
+                $comm_id = (int) Yii::app()->request->getParam( "comm_id", 0 );
+                if( !empty($action) && $comm_id>0 )
                 {
-                    if( $action == "delComment" )
+                    $comModel = CatComments::fetch( $comm_id );
+                    if( $comModel->id >0 && $comModel->item_id->id == $item->id )
                     {
-                        $comModel->delete();
-                        $comMessage = "Коментарий удален";
-                    }
+                        if( $action == "delComment" )
+                        {
+                            $comModel->delete();
+                            $comMessage = "Коментарий удален";
+                        }
 
-                    if( $action == "validComment" )
-                    {
-                        $comModel->is_valid = 1;
-                        $comModel->save();
-                        $comMessage = "Коментарий успешно опубликован";
+                        if( $action == "validComment" )
+                        {
+                            $comModel->is_valid = 1;
+                            $comModel->save();
+                            $comMessage = "Коментарий успешно опубликован";
+                        }
                     }
                 }
-            }
 
-            $addImage = new CatGalleryAdd();
-            if( $error == "gallError" )$addImage->addError( "error upload", "Произошла ошибка добавления фото, попробуте заново или обратитеcь к тех. потдержке ( Email : ".Yii::app()->params["supportEmail"]." ) " );
-            if( !empty( $_POST["sendGallery"] ) )
-            {
-                if( $id>0 )$this->uploadImages( (int) $id, get_class( $item ) );
-            }
+                $addImage = new CatGalleryAdd();
+                if( $error == "gallError" )$addImage->addError( "error upload", "Произошла ошибка добавления фото, попробуте заново или обратитеcь к тех. потдержке ( Email : ".Yii::app()->params["supportEmail"]." ) " );
+                if( !empty( $_POST["sendGallery"] ) )
+                {
+                    if( $id>0 )$this->uploadImages( (int) $id, get_class( $item ) );
+                }
 
-            // Сохранение подписи для фотографий
-            if( !empty( $_POST["saveTitle"] ) )$this->gallerySaveTitle();
+                // Сохранение подписи для фотографий
+                if( !empty( $_POST["saveTitle"] ) )$this->gallerySaveTitle();
 
-            $listComments = CatComments::fetchAll( DBQueryParamsClass::CreateParams()->setConditions("catalog=:catalog AND item_id=:item_id")->setParams( array( ":catalog"=>$item->tableName(), ":item_id"=>$item->id ) )->setLimit(50)->setCache(0) );
-            $listGallery = CatGallery::fetchAll( DBQueryParamsClass::CreateParams()->setConditions("catalog=:catalog AND item_id=:item_id")->setParams( array( ":catalog"=>$item->tableName(), ":item_id"=>$item->id ) )->setLimit(50)->setCache(0) );
+                $listComments = CatComments::fetchAll( DBQueryParamsClass::CreateParams()->setConditions("catalog=:catalog AND item_id=:item_id")->setParams( array( ":catalog"=>$item->tableName(), ":item_id"=>$item->id ) )->setLimit(50)->setCache(0) );
+                $listGallery = CatGallery::fetchAll( DBQueryParamsClass::CreateParams()->setConditions("catalog=:catalog AND item_id=:item_id")->setParams( array( ":catalog"=>$item->tableName(), ":item_id"=>$item->id ) )->setLimit(50)->setCache(0) );
 
 
-            $this->render( "description", array( "item"=>$item, "firm"=>$firm, "listGallery"=>$listGallery, "message"=>$message, "addImage"=>$addImage, "comMessage"=>$comMessage, "gallMessage"=>$gallMessage, "listComments"=>$listComments ) );
+                $this->render( "description", array( "item"=>$item, "firm"=>$firm, "listGallery"=>$listGallery, "message"=>$message, "addImage"=>$addImage, "comMessage"=>$comMessage, "gallMessage"=>$gallMessage, "listComments"=>$listComments ) );
+        }
         }
     }
 }
--- protected/modules/user/controllers/ItemsController.php
+++ protected/modules/user/controllers/ItemsController.php
@@ -29,87 +29,91 @@ class ItemsController extends UserController
             $status = Yii::app()->request->getParam("status", "");
             $error = Yii::app()->request->getParam("error", "");
             $addClass = $this->addModel;
+
             if( !empty( $id ) )$item = $addClass::fetch( $id );
-                else $item = new $addClass();
+                     else $item = new $addClass();
 
-            $message = ( !empty( $status ) && $status == 'saved' ) ? "Сохраненно" : "";
-
-            // Описание объявления
-            if( !empty( $_POST["update"] ) )
+            if( !$item->id || $item->user_id->id == Yii::app()->user->getId()  )
             {
-                if( !$item->id )$isAdd = true;
-                else $isAdd = false;
+                $message = ( !empty( $status ) && $status == 'saved' ) ? "Сохраненно" : "";
 
-                $item->setAttributesFromArray( $_POST[ $addClass ] );
-                //$item->is_resume = 0;
-                if( !$item->date )$item->date = time();
-                if( !$item->id )$item->user_id = Yii::app()->user->getId();
-
-                if( $item->save() )
+                // Описание объявления
+                if( !empty( $_POST["update"] ) )
                 {
-                    $this->redirect( SiteHelper::createUrl( "/user/".Yii::app()->controller->getId()."/description/", array("id"=>$item->id, "status"=>"saved") ) );
-                    die;
-                    //if( !$isAdd )$message = "Описание успешно обновленно";
-                    //        else $message = "Запись успешно добавлена";
-                }
-//                    else $message = "Произошла ошибка обновления описания";
-            }
+                    if( !$item->id )$isAdd = true;
+                    else $isAdd = false;
 
-            $action = Yii::app()->request->getParam( "action" );
-            $gall_id = (int) Yii::app()->request->getParam( "gall_id", 0 );
-            $comMessage = "";
-            $gallMessage = "";
-            if( !empty($gallError) )$message = $gallError;
-            // Удаление фотографии
-            if( !empty($action) && $gall_id>0 )
-            {
-                $comModel = CatGallery::fetch( $gall_id );
-                if( $comModel->id >0 && $comModel->item_id == $item->id )
-                {
-                    if( $action == "delGallery" )
+                    $item->setAttributesFromArray( $_POST[ $addClass ] );
+                    //$item->is_resume = 0;
+                    if( !$item->date )$item->date = time();
+                    if( !$item->id )$item->user_id = Yii::app()->user->getId();
+
+                    if( $item->save() )
                     {
-                        $comModel->delete();
-                        $gallMessage = "Картинка удалена";
+                        $this->redirect( SiteHelper::createUrl( "/user/".Yii::app()->controller->getId()."/description/", array("id"=>$item->id, "status"=>"saved") ) );
+                        die;
+                        //if( !$isAdd )$message = "Описание успешно обновленно";
+                        //        else $message = "Запись успешно добавлена";
+                    }
+    //                    else $message = "Произошла ошибка обновления описания";
+                }
+
+                $action = Yii::app()->request->getParam( "action" );
+                $gall_id = (int) Yii::app()->request->getParam( "gall_id", 0 );
+                $comMessage = "";
+                $gallMessage = "";
+                if( !empty($gallError) )$message = $gallError;
+                // Удаление фотографии
+                if( !empty($action) && $gall_id>0 )
+                {
+                    $comModel = CatGallery::fetch( $gall_id );
+                    if( $comModel->id >0 && $comModel->item_id == $item->id )
+                    {
+                        if( $action == "delGallery" )
+                        {
+                            $comModel->delete();
+                            $gallMessage = "Картинка удалена";
+                        }
                     }
                 }
-            }
 
-            $comm_id = (int) Yii::app()->request->getParam( "comm_id", 0 );
-            if( !empty($action) && $comm_id>0 )
-            {
-                $comModel = CatComments::fetch( $comm_id );
-                if( $comModel->id >0 && $comModel->item_id->id == $item->id )
+                $comm_id = (int) Yii::app()->request->getParam( "comm_id", 0 );
+                if( !empty($action) && $comm_id>0 )
                 {
-                    if( $action == "delComment" )
+                    $comModel = CatComments::fetch( $comm_id );
+                    if( $comModel->id >0 && $comModel->item_id->id == $item->id )
                     {
-                        $comModel->delete();
-                        $comMessage = "Коментарий удален";
-                    }
+                        if( $action == "delComment" )
+                        {
+                            $comModel->delete();
+                            $comMessage = "Коментарий удален";
+                        }
 
-                    if( $action == "validComment" )
-                    {
-                        $comModel->is_valid = 1;
-                        $comModel->save();
-                        $comMessage = "Коментарий успешно опубликован";
+                        if( $action == "validComment" )
+                        {
+                            $comModel->is_valid = 1;
+                            $comModel->save();
+                            $comMessage = "Коментарий успешно опубликован";
+                        }
                     }
                 }
+
+                $addImage = new CatGalleryAdd();
+                if( $error == "gallError" )$addImage->addError( "error upload", "Произошла ошибка добавления фото, попробуте заново или обратитеcь к тех. потдержке ( Email : ".Yii::app()->params["supportEmail"]." ) " );
+                if( !empty( $_POST["sendGallery"] ) )
+                {
+                    if( $id>0 )$this->uploadImages( (int) $id, get_class( $item ) );
+                }
+
+                // Сохранение подписи для фотографий
+                if( !empty( $_POST["saveTitle"] ) )$this->gallerySaveTitle();
+
+                $listComments = CatComments::fetchAll( DBQueryParamsClass::CreateParams()->setConditions("catalog=:catalog AND item_id=:item_id")->setParams( array( ":catalog"=>$item->tableName(), ":item_id"=>$item->id ) )->setLimit(50)->setCache(0) );
+                $listGallery = CatGallery::fetchAll( DBQueryParamsClass::CreateParams()->setConditions("catalog=:catalog AND item_id=:item_id")->setParams( array( ":catalog"=>$item->tableName(), ":item_id"=>$item->id ) )->setLimit(50)->setCache(0) );
+
+
+                $this->render( "description", array( "item"=>$item, "firm"=>null, "listGallery"=>$listGallery, "message"=>$message, "addImage"=>$addImage, "comMessage"=>$comMessage, "gallMessage"=>$gallMessage, "listComments"=>$listComments ) );
             }
-
-            $addImage = new CatGalleryAdd();
-            if( $error == "gallError" )$addImage->addError( "error upload", "Произошла ошибка добавления фото, попробуте заново или обратитеcь к тех. потдержке ( Email : ".Yii::app()->params["supportEmail"]." ) " );
-            if( !empty( $_POST["sendGallery"] ) )
-            {
-                if( $id>0 )$this->uploadImages( (int) $id, get_class( $item ) );
-            }
-
-            // Сохранение подписи для фотографий
-            if( !empty( $_POST["saveTitle"] ) )$this->gallerySaveTitle();
-
-            $listComments = CatComments::fetchAll( DBQueryParamsClass::CreateParams()->setConditions("catalog=:catalog AND item_id=:item_id")->setParams( array( ":catalog"=>$item->tableName(), ":item_id"=>$item->id ) )->setLimit(50)->setCache(0) );
-            $listGallery = CatGallery::fetchAll( DBQueryParamsClass::CreateParams()->setConditions("catalog=:catalog AND item_id=:item_id")->setParams( array( ":catalog"=>$item->tableName(), ":item_id"=>$item->id ) )->setLimit(50)->setCache(0) );
-
-
-            $this->render( "description", array( "item"=>$item, "firm"=>null, "listGallery"=>$listGallery, "message"=>$message, "addImage"=>$addImage, "comMessage"=>$comMessage, "gallMessage"=>$gallMessage, "listComments"=>$listComments ) );
         }
     }
 }
